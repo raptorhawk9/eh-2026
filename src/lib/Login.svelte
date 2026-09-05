b0fd5fc8dca1a298a147ae9391a199caa464ccb8
raptorhawk9 <142059090+raptorhawk9@users.noreply.github.com>
2026-09-05 14:08:32 -0700

    first pass of login page

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
-    
+	let props = $props();
 </script>
 
-<div class="bg-neutral-700 w-full flex flex-col">
-    stuff
+<div class="flex min-w-1/3 flex-col justify-center gap-4">
+	<h1 class="text-6xl text-white">Login</h1>
+	<form class="flex flex-col justify-center gap-4" method="POST">
+		<label class="text-white" for="username">Username</label>
+		<input id="username" type="text" />
+        <label class="text-white" for="password">Password</label>
+		<input id="password" type="password" />
+        <button class="text-white border-white border hover:cursor-pointer">Submit</button>
+	</form>
 </div>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import Login from '$lib/Login.svelte';
+
+    let props = $props();
 </script>
 
-<div class="w-full h-full">
+<div class="w-screen h-screen flex flex-col items-center justify-center bg-neutral-900">
     <Login/>
 </div>
