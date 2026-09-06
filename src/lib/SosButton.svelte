b4474e925ae87a3afd1c710f61062cffd34214d7
raptorhawk9 <142059090+raptorhawk9@users.noreply.github.com>
2026-09-06 09:01:06 -0700

    Add SOS button

---
+++ src/lib/SosButton.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    function confirmSOS() {
+        confirm("SOS Message Sent");
+    }
+</script>
+
+<div class="fixed top-6 right-6 z-50 flex items-center justify-center">
+	<div class="border border-white p-4 bg-neutral-900">
+		<button class="bg-red-700 p-4 text-2xl hover:cursor-pointer" onclick={confirmSOS}>SOS</button>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
 	import Login from '$lib/Login.svelte';
+	import SosButton from '$lib/SosButton.svelte';
 	/** @type {import('./$types').PageProps} */
 	let { data } = $props();
 </script>
 
 {#if !data.user}
-	<div class="flex h-screen w-screen flex-col items-center justify-center bg-neutral-900">
+	<div class="flex h-screen w-screen flex-col items-center justify-center">
 		<Login />
 	</div>
 {:else}
-	<div class="h-screen w-screen bg-neutral-900 text-white">Main Dashboard</div>
+	<div class="h-screen w-screen text-white">
+		<SosButton />
+		<p class="wrap-break-word">stuff</p>
+	</div>
 {/if}
